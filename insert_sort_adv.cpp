64dca11f16313df0dde8b8fe32e41a91318cfeca
David Citrix Rock <shihao.zhang@citrix.com>
2016-06-25 22:02:54 -0400

    fixing errors

--- insert_sort_adv.cpp
+++ insert_sort_adv.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class node {
 private:
     /* data */
@@ -5,35 +10,37 @@ private:
     node* left, *right, *parent;
 public:
     node (int value){
-        this.value = value;
+        this->value = value;
         sum_left = sum_right = 0;  sum_here = 1;
         left = right = parent = nullptr;
     }
-    virtual ~node ();
-
+    
     int cmp(int val){
-        if (val > this.value) {return 1;}
-        else if(val == this.value){return 0;}
+        if (val > this->value) {return 1;}
+        else if(val == this->value){return 0;}
         else{return -1;}
     }
-    void set_left(node* input){this.left = input;}
-    void set_right(node* input){this.right = input;}
-    void set_parent(node* input){this.parent = input;}
-    node* go_left(){return this.left;}
-    node* go_right(){return this.right;}
-    void add_left(){this.sum_left++;}
-    void add_right(){this.sum_right++;}
-    void add_here(){this.sum_here++;}
-    int get_right_sum(){return this.sum_right;}
-    int get_left_sum(){Return this.sum_left;}
+    void set_left(node* input){this->left = input;}
+    void set_right(node* input){this->right = input;}
+    void set_parent(node* input){this->parent = input;}
+    node* go_left(){return this->left;}
+    node* go_right(){return this->right;}
+    void add_left(){this->sum_left++;}
+    void add_right(){this->sum_right++;}
+    void add_here(){this->sum_here++;}
+    int get_right_sum(){return this->sum_right;}
+    int get_left_sum(){return this->sum_left;}
+    int get_here_sum(){return this->sum_here;}
+    int get_value(){return this->value;}
 };
 
 void add_value(int value, node* root){
     int count = 0;
     node* curr_node = root, *parent = nullptr;
     while(curr_node != nullptr){
-        count += curr_node->sum_right;
         /* do counting + increment sum */
+        count += curr_node->get_right_sum();
+        parent = curr_node;                     // record parent
         int cmp = curr_node->cmp(value);
         if(cmp == 0){
             // found it
@@ -43,13 +50,28 @@ void add_value(int value, node* root){
         }
         else if(cmp == 1){
             // value > curr_node
-            count -= curr_node->sum_right;
-            curr_node->add_right();
-            curr_node = curr_node->go_right();
+            count -= curr_node->get_right_sum();    // reset count
+            curr_node->add_right();                 // increment right sum
 
+            curr_node = curr_node->go_right();      // move right
+        }
+        else{
+            // value < curr_node
+            curr_node->add_left();                  // increment left sum
+            count += curr_node->get_here_sum();     // add the curr_node to count
+            curr_node = curr_node->go_left();       // move left
         }
     }
     cout << count << "\n";
     curr_node = new node(value);
-    curr_node.parent = nullptr;
+    curr_node->set_parent(parent);
+}
+
+int main(){
+    vector<int> test = {2, 1, 3, 1, 2};
+    node* root = new node(test[0]);
+    for(int i = 1; i < test.size(); i++){
+        add_value(test[i], root);
+    }
+    return 0;
 }
