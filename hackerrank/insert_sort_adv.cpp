d1e3294dc0ba9f01f0c5c345bd5910546adc4206
David Citrix Rock <shihao.zhang@citrix.com>
2016-06-25 22:44:56 -0400

    need a balancing tree

--- hackerrank/insert_sort_adv.cpp
+++ hackerrank/insert_sort_adv.cpp
@@ -13,10 +13,10 @@ vector<int> readvector(){
 class node {
 private:
     /* data */
-    int value, sum_left, sum_right, sum_here;
+    long value; long sum_left, sum_right, sum_here;
     node* left, *right, *parent;
 public:
-    node (int value){
+    node (long value){
         this->value = value;
         sum_left = sum_right = 0;  sum_here = 1;
         left = right = parent = nullptr;
@@ -35,14 +35,14 @@ public:
     void add_left(){this->sum_left++;}
     void add_right(){this->sum_right++;}
     void add_here(){this->sum_here++;}
-    int get_right_sum(){return this->sum_right;}
-    int get_left_sum(){return this->sum_left;}
-    int get_here_sum(){return this->sum_here;}
+    long get_right_sum(){return this->sum_right;}
+    long get_left_sum(){return this->sum_left;}
+    long get_here_sum(){return this->sum_here;}
     int get_value(){return this->value;}
 };
 
-int add_value(int value, node* root){
-    int count = 0;
+long add_value(long value, node* root){
+    long count = 0;
     node* curr_node = root, *parent = nullptr;
     int cmp = 0;
     while(curr_node != nullptr){
@@ -82,10 +82,10 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int num = 0;    cin >> num;
     for(int i = 0; i < num; i++){
-        vector<int> arr = readvector();
-        int count = 0;
+        vector<long> arr = readvector();
+        long count = 0;
         node* root = new node(arr[0]);
-        for(int idx = 1; idx < arr.size(); idx++){
+        for(long idx = 1; idx < arr.size(); idx++){
             count += add_value(arr[idx], root);
         }
         cout << count << endl;
